6747443a8af37804cd0215ad542424678f6e9147
Miguel Ping <miguel.ping@gmail.com>
2020-05-02 15:29:44 +0100

    upgrade electron, add data spec

--- src/observideo/common/datamodel.cljs
+++ src/observideo/common/datamodel.cljs
@@ -1,4 +1,6 @@
-(ns observideo.common.datamodel)
+(ns observideo.common.datamodel
+  (:require [spec-tools.data-spec :as ds]
+            [clojure.spec.alpha :as s]))
 
 (def demo-template {:id         "fb52dd46-85cc-4864-b11e-44b8a5b28331"
                     :name       "Demo"
@@ -7,6 +9,8 @@
                     :attributes {"Peer"   {:index 0 :values ["Alone" "Adults" "Peers" "Adults and Peers" "N/A"]}
                                  "Gender" {:index 1 :values ["Same" "Opposite" "Both" "N/A"]}
                                  "Type"   {:index 2 :values ["Roleplay" "Rough and Tumble" "Exercise"]}}})
+
+
 
 (def demo-video {:filename        "/home/mping/Download … deo_720x480_30mb.mp4"
                  :duration        183.318
@@ -18,15 +22,49 @@
                  :observations    [{"Peer" nil "Gender" "Same" "Type" "Exercise"}, {}]
                  :template-id     "7dd2479d-e829-4762-a0ac-de51a68461b5"})
 
+;;;;
+;; Specs
+
+(def attribute-spec
+  (ds/spec {:name ::atribute
+            :spec {:index int? :values [string?]}}))
+
+(def template-spec
+  (ds/spec {:name ::template
+            :spec {:id         string?
+                   :name       string?
+                   :interval   int?
+                   :next-index int?
+                   :attributes (s/map-of string? attribute-spec)}}))
+
+(def section-spec
+  (ds/spec {:name ::section
+            :spec {:time number? :index int?}}))
+
+(def observation-spec
+  (ds/spec {:name ::observation
+            :spec (s/map-of string? (s/nilable string?))}))
+
+(def video-spec
+  (ds/spec {:name ::video
+            :spec {:filename        string?
+                   :duration        number?
+                   :info            any?
+                   :md5sum          string?
+                   :size            int?
+                   :missing?        boolean?
+                   :current-section section-spec
+                   :observations    [observation-spec]
+                   :template-id     string?}}))
 
 (defn empty-db []
   {:observideo/filename nil
    :ui/tab              :videos
 
    ;; videos list is a vec because they are in the filesystem
-   :videos/folder       nil                                 ;;string
-   :videos/all          nil                                 ;;map {filename > video}
-   :videos/current      nil                                 ;;video
+   :videos/folder       nil ;;string
+   :videos/all          nil ;;map {filename > video}
+   :videos/current      nil ;;video
 
    ;; templates are keyed by :id because it facilitates CRUD operations
    :templates/all       {(:id demo-template) demo-template} ;; {uuid -> template}
--- src/observideo/renderer/components/video_edit.cljs
+++ src/observideo/renderer/components/video_edit.cljs
@@ -57,7 +57,7 @@
 
                  [:tr {:key rowkey}
                   [:td {:key     tdkey
-                        :style {:background (when attribute-on? "red")}
+                        :style {:background (when attribute-on? "#1890ff")}
                         :onClick #(rf/dispatch [:ui/update-current-video-current-section-observation
                                                 (assoc observation header attribute)])}
                    attribute]])]]])]]]]]]))
